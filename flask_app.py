adcd1f2a3f7306f6df942a68322129e01d202e71
ewxve <nikeneikirk@gmail.com>
2024-09-20 14:52:37 -0600

    3-long emoji -5 x

--- flask_app.py
+++ flask_app.py
@@ -34,6 +34,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #from waitress import serve
-    #serve(app, host="0.0.0.0", port=8080)
+    #app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
